63ee06caf3e51aa784731bbd0bbf1836c419ad1e
r.rajujakhar1993@gmail.com <pi02dawu@stud.informatik.uni-erlangen.de>
2016-07-03 16:12:10 +0200

    removed folder openLB

--- doc/olb-0.7r1/src/olb3D.hh
+++
@@ -1,9 +0,0 @@
-#include <core/core3D.hh>
-#include <boundaryConditions/boundaryConditions3D.hh>
-#include <complexDynamics/complexDynamics3D.hh>
-#include <io/io3D.hh>
-#include <utilities/utilities3D.hh>
-#include <complexGrids/cuboidStructure/cuboidStructure3D.hh>
-#include <complexGrids/multiBlockStructure/multiBlockStructure3D.hh>
-
-
